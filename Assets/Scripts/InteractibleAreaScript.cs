fbc332843a10e40b3766c62a8085245f04728ae5
Julia-Wag <juli.w.081009@gmail.com>
2026-04-29 14:19:19 -0300

    Initial commit

---
+++ Assets/Scripts/InteractibleAreaScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractibleAreaScript : MonoBehaviour
+{
+    public ScoreUIManager scoreUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("Picable"))
+        {
+            Destroy(col.gameObject);
+            scoreUI.AddPointAndUpdateScore();
+        }
+    }
+}
